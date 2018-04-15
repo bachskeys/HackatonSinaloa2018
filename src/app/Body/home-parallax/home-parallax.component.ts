23caf1230442c0c134bded7ec6761869da251e17
Unknown <davidmv1295@gmail.com>
2018-04-15 16:20:42 -0600

    Corrigiendo query ejemplo

--- src/app/Body/home-parallax/home-parallax.component.ts
+++ src/app/Body/home-parallax/home-parallax.component.ts
@@ -4,8 +4,8 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-import { Observable } from 'rxjs/Observable';
 import { AngularFirestore,AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 
 
@@ -16,23 +16,20 @@ import { AngularFirestore,AngularFirestoreCollection } from 'angularfire2/firest
 	styleUrls: ['./home-parallax.component.css']
 })
 export class HomeParallaxComponent implements OnInit {
-
-	
-	constructor() {
+	items: Observable<any[]>;
+	habitaciones: Observable<any[]>;
+	reviews: Observable<any[]>;
+	constructor(private db: AngularFirestore) {
 
 		
 	}
 
 	ngOnInit() {
-		
+		this.items = this.db.collection('Hoteles').valueChanges();
+		this.habitaciones = this.db.collection('Habitaciones', ref => ref.where('Id_Hotel', '==', 1)).valueChanges();
+		this.reviews = this.db.collection('Review', ref => ref.where('Id_Hotel', '==', 1)).valueChanges();
 	}
 
-	items: Observable<any[]>;
-	habitaciones: Observable<any[]>;
-	reviews: Observable<any[]>;
-	db: AngularFirestore;
-	items = this.db.collection('Hoteles').valueChanges();
-	habitaciones = this.db.collection('Habitaciones', ref => ref.where('Id_Hotel', '==', 1)).valueChanges();
-	reviews = this.db.collection('Review', ref => ref.where('Id_Hotel', '==', 1)).valueChanges();
+	
 	
 }
