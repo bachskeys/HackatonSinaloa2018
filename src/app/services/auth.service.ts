23ef271e0dd740a81fbeb71dd25d0f528b63e98c
Unknown <davidmv1295@gmail.com>
2018-04-17 17:11:04 -0600

    Meter usuer a la BD

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,13 +4,14 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 
 import { Observable } from 'rxjs/Observable';
+import { AngularFirestore} from 'angularfire2/firestore';
 
 @Injectable()
 export class AuthService {
   user: Observable<firebase.User>;
   userFlag:boolean;
 
-  constructor(private firebaseAuth: AngularFireAuth) {
+  constructor(private firebaseAuth: AngularFireAuth, private db: AngularFirestore) {
     this.user = firebaseAuth.authState;
   }
 
@@ -21,6 +22,8 @@ export class AuthService {
       .then(value => {
         console.log('Success!', value);
         this.userFlag=true;
+        let newUser = this.db.collection<any>('Usuario');
+        newUser.add({email:email,password:password, Imagen: "https://firebasestorage.googleapis.com/v0/b/hackaton-17e3d.appspot.com/o/default-profile.png?alt=media&token=c68bc8b8-3da1-4cda-a89f-41cac7fd928b"});
       })
       .catch(err => {
         console.log('Something went wrong:',err.message);
@@ -43,7 +46,7 @@ export class AuthService {
   logout() {
     this.firebaseAuth
     .auth.signOut().then(value => {
-    console.log('Sing out sucessful' value);
+    console.log('Sing out sucessful', value);
     this.userFlag=false;
 }).catch(function(error) {
   // An error happened.
