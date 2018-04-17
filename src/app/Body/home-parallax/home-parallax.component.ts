71206610d53686c9f0b2d1669911524e3168730f
Unknown <16171281@itculiacan.edu.mx>
2018-04-17 03:24:14 -0600

    login with FaceBook and Email in the Modal!

--- src/app/Body/home-parallax/home-parallax.component.ts
+++ src/app/Body/home-parallax/home-parallax.component.ts
@@ -6,6 +6,7 @@ import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFirestore,AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
+import { AuthService } from 'app/services/auth.service';
 
 
 
@@ -15,24 +16,34 @@ import { Observable } from 'rxjs/Observable';
 	templateUrl: './home-parallax.component.html',
 	styleUrls: ['./home-parallax.component.css']
 })
-export class HomeParallaxComponent implements OnInit {
-	items: Observable<any[]>;
-	habitaciones: Observable<any[]>;
-	reviews: Observable<any[]>;
-	constructor(private db: AngularFirestore) {
+export class HomeParallaxComponent  {
+	
+	 email: string;
+  password: string;
 
-		
-	}
+  constructor(public authService: AuthService) {}
 
-	ngOnInit() {
-		this.items = this.db.collection('Hoteles').valueChanges();
-		this.habitaciones = this.db.collection('Habitaciones', ref => ref.where('Id_Hotel', '==', 1)).valueChanges();
-		this.reviews = this.db.collection('Review', ref => ref.where('Id_Hotel', '==', 1)).valueChanges();
-	}
+  signup() {
+    this.authService.signup(this.email, this.password);
+    this.email = this.password = '';
+  }
+
+  login() {
+    this.authService.login(this.email, this.password);
+    this.email = this.password = '';    
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+  loginWithFaceBook(){
+
+
+   this.authService.loginWithFaceBook();
+
+
+  }
 
 	
-	getUrl()
-{
-  return "url('assets/img/OCRS5G0.jpg')";
-}
+	
 }
