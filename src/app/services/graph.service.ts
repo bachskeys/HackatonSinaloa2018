75089fddafe9ce181a30407bf928781a993d7ea9
Unknown <davidmv1295@gmail.com>
2018-04-16 19:48:56 -0600

    creando servicio http

---
+++ src/app/services/graph.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+
+const appToken="205481190051738|0OpRnkX13GQBmjOBnJB7bQGRlwo";
+
+@Injectable()
+export class GraphService {
+
+	constructor() {
+	}
+
+}
